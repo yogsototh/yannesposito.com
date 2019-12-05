5435e583eb3aa632c6690b99108cf32ae9ff563d
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2019-12-05 17:17:31 +0100

    slightly safer script

--- optim-classes.sh
+++ optim-classes.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-classes=( $( {cat _site/**/*.html | perl -p -e 's/class="?([a-zA-Z0-9_-]*)/\nCLASS: $1\n/g'; cat _site/**/*.css | perl -p -e 's/\.([a-zA-Z-_][a-zA-Z0-9-_]*)/\nCLASS: $1\n/g'}|grep CLASS|sort -u|cut -d\   -f 2,2|awk 'length($1)>2 {print length($1),$1}'|sort -n|cut -d\  -f 2,2) )
+classes=( $( {cat _site/**/*.html(N) | perl -p -e 's/class="?([a-zA-Z0-9_-]*)/\nCLASS: $1\n/g'; cat _site/**/*.css(N) | perl -p -e 's/\.([a-zA-Z-_][a-zA-Z0-9-_]*)/\nCLASS: $1\n/g'}|grep CLASS|sort -u|cut -d\   -f 2,2|awk 'length($1)>2 {print length($1),$1}'|sort -n|cut -d\  -f 2,2) )
 
 chr() {
     [ "$1" -lt 26 ] || return 1
@@ -25,13 +25,13 @@ for c in $classes; do
 done
 
 
-for fic in _site/**/*.html; do
+for fic in _site/**/*.{html,xml}(N); do
     print -- $fic
     for long in $classes; do
         perl -pi  -e 's#class=("?)'${long}'#class=$1'${assoc[$long]}'#g' $fic
     done
 done
-for fic in _site/**/*.css; do
+for fic in _site/**/*.css(N); do
     echo $fic
     for long in $classes; do
         perl -pi  -e 's#\.'"${long}"'#.'"${assoc[$long]}"'#g' $fic
