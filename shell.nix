e7015adbb09af6b2506a0ce4948fc5e0ef2b220d
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2019-12-16 13:07:02 +0100

    minor progress around code source

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 # { pkgs ? import <nixpkgs> {} }:
-{ pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/19.09-beta.tar.gz) {} }:
+{ pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/19.09.tar.gz) {} }:
   pkgs.mkShell {
     buildInputs = [ pkgs.coreutils
                     pkgs.html-xml-utils
