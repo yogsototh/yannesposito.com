c65b820e0ab3ecbe0320f862c20402226b31d534
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2019-12-27 11:30:08 +0100

    Tryed to add more insight for Haskell types.

--- src/posts/0010-Haskell-Now/infinite_tree.hs
+++ src/posts/0010-Haskell-Now/infinite_tree.hs
@@ -1,21 +1,21 @@
-  import           Data.Tree (Tree,Forest(..))
-  import qualified Data.Tree as Tree
+import           Data.Tree (Tree,Forest(..))
+import qualified Data.Tree as Tree
 
-  data BinTree a = Empty
-                 | Node a (BinTree a) (BinTree a)
-                 deriving (Eq,Ord,Show)
+data BinTree a = Empty
+               | Node a (BinTree a) (BinTree a)
+               deriving (Eq,Ord,Show)
 
-  -- | Function to transform our internal BinTree type to the
-  -- type of Tree declared in Data.Tree (from containers package)
-  -- so that the function Tree.drawForest can use
-  binTreeToForestString :: (Show a) => BinTree a -> Forest String
-  binTreeToForestString Empty = []
-  binTreeToForestString (Node x left right) =
-    [Tree.Node (show x) ((binTreeToForestString left) ++ (binTreeToForestString right))]
+-- | Function to transform our internal BinTree type to the
+-- type of Tree declared in Data.Tree (from containers package)
+-- so that the function Tree.drawForest can use
+binTreeToForestString :: (Show a) => BinTree a -> Forest String
+binTreeToForestString Empty = []
+binTreeToForestString (Node x left right) =
+  [Tree.Node (show x) ((binTreeToForestString left) ++ (binTreeToForestString right))]
 
-  -- | Function that given a BinTree print a representation of it in the console
-  prettyPrintTree :: (Show a) => BinTree a -> IO ()
-  prettyPrintTree = putStrLn . Tree.drawForest . binTreeToForestString
+-- | Function that given a BinTree print a representation of it in the console
+prettyPrintTree :: (Show a) => BinTree a -> IO ()
+prettyPrintTree = putStrLn . Tree.drawForest . binTreeToForestString
 
 nullTree = Node 0 nullTree nullTree
 
