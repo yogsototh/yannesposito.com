d817cc36759bc46d6922fbd49a71f7d4f8f57fba
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2020-04-11 14:29:35 +0200

    cleaner init/clean

--- engine/clean.sh
+++ engine/clean.sh
@@ -2,8 +2,8 @@
 
 cd "$(git rev-parse --show-toplevel)" || exit 1
 echo -n "* Clean site cache"
-find _site -mindepth 1 -delete
-find _full -mindepth 1 -delete
-find _optim -mindepth 1 -delete
+find _site -not -path "_site/.gitignore" -mindepth 1 -delete
+find _full -not -path "_full/.gitignore" -mindepth 1 -delete
+find _optim -not -path "_optim/.gitignore" -mindepth 1 -delete
 rm -rf _cache
 echo " [done]"
