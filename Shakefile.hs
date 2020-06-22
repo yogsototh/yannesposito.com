b81339a51e396b58ed8c2ae412b5d58dbba38c9f
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2020-06-22 12:08:07 +0200

    progress

--- Shakefile.hs
+++ Shakefile.hs
@@ -32,12 +32,24 @@ import qualified Text.Pandoc.Readers           as Readers
 import qualified Text.Pandoc.Writers           as Writers
 
 main :: IO ()
-main = do
-        let
-                shOpts = shakeOptions { shakeVerbosity  = Chatty
-                                      , shakeLintInside = ["\\"]
-                                      }
-        shakeArgs shOpts buildRules
+main = shakeArgs shOpts buildRules
+  where
+    shOpts =
+      shakeOptions
+      { shakeVerbosity  = Chatty
+      , shakeLintInside = ["\\"]
+      }
+
+-- Configuration
+-- Should probably go in a Reader Monad
+
+siteDir :: FilePath
+siteDir  = "_site"
+
+optimDir :: FilePath
+optimDir = "_optim"
+
+-- BlogPost data structure (a bit of duplication because the metas are in Pandoc)
 
 data BlogPost =
   BlogPost { postTitle :: T.Text
@@ -66,73 +78,39 @@ getBlogpostFromMetas path pandoc@(Pandoc meta _) = do
 
 sortByPostDate :: [BlogPost] -> [BlogPost]
 sortByPostDate =
-        sortBy (\a b -> compare (Down (postDate a)) (Down (postDate b)))
+        sortBy (\b a -> compare (Down (postDate a)) (Down (postDate b)))
 
 buildRules :: Rules ()
 buildRules = do
-        let     siteDir  = "_site"
-                optimDir = "_optim"
-                build    = (</>) siteDir
-        phony "clean" $ do
-                putInfo "Cleaning files in _site and _optim"
-                removeFilesAfter siteDir  ["//*"]
-                removeFilesAfter optimDir ["//*"]
-        getPost <- newCache $ \path -> do
-                fileContent  <- readFile' path
-                eitherResult <- liftIO $ Pandoc.runIO $ Readers.readOrg
-                        def
-                        (T.pack fileContent)
-                case eitherResult of
-                        Left  _      -> fail "BAD"
-                        Right pandoc -> getBlogpostFromMetas path pandoc
-        getPosts <-
-                newCache
-                        $ \() -> mapM getPost =<< getDirectoryFiles
-                                  ""
-                                  ["src/posts//*.org"]
-        let     -- hsDeps  = return ["AsciiArt.hs", "Index.hs", "Rot13.hs"]
-                cssDeps = map (siteDir </>)
-                        <$> getDirectoryFiles "" ["src/css/*.css"]
+        let build = (</>) siteDir
+        cleanRule
+        getPost <- mkGetPost
+        getPosts <- mkGetPosts getPost
+        let cssDeps = map (siteDir </>) <$> getDirectoryFiles "" ["src/css/*.css"]
         build "index.html" %> \out -> do
-                -- hs    <- hsDeps
                 css   <- cssDeps
                 posts <- getPosts ()
-                need $ css <> map postUrl posts
-                  -- <> [build "atom.xml"]
-                let titles = map postTitle posts
-                writeFile' out (mconcat (map T.unpack titles))
+                need $ css <> map postUrl (sortByPostDate  posts)
+                let titles = T.unpack $ T.intercalate "\n" $ map postTitle posts
+                writeFile' out titles
+        -- build "//*.html" %> \out -> do
+        --   css <- cssDeps
+        --   let orgfile = dropDirectory1 out
+        --   post <- getPost orgfile
         build "src/css/*.css" %> \out -> copyFile' (dropDirectory1 out) out
 
+cleanRule :: Rules ()
+cleanRule =
+  phony "clean" $ do
+    putInfo "Cleaning files in _site and _optim"
+    forM_ [siteDir,optimDir] $ flip removeFilesAfter ["//*"]
 
---  "_site//*.html" %> buildPost
---  buildPosts
---  allPosts <- buildPosts
---  buildIndex allPosts
---  buildFeed allPosts
---  copyStaticFiles
+mkGetPost = newCache $ \path -> do
+  fileContent  <- readFile' path
+  eitherResult <- liftIO $ Pandoc.runIO $ Readers.readOrg def (T.pack fileContent)
+  case eitherResult of
+    Left  _      -> fail "BAD"
+    Right pandoc -> getBlogpostFromMetas path pandoc
 
--- data Post = Post { postTitle :: T.Text
---                  , postAuthor :: T.Text
---                  , postDate :: T.Text
---                  }
--- 
--- defaultReaderOpts t =
---   def { readerExtensions = getDefaultExtensions t
---       , readerStandalone = True }
--- 
--- orgToHTML :: T.Text -> PandocPure T.Text
--- orgToHTML txt = Readers.readOrg (defaultReaderOpts "org") txt
---   >>= Writers.writeHtml5String def
--- 
--- -- | Load a post, process metadata, write it to output, then return the post object
--- -- Detects changes to either post content or template
--- buildPost :: FilePath -> Action ()
--- buildPost out = do
---   let org = "src/" <> (dropDirectory1 $ out -<.> "org")
---   liftIO . putStrLn $ "Rebuilding post: " <> out
---   postContent <- readFile' org
---   -- load post content and metadata as JSON blob
---   let pandocReturn = Pandoc.runPure $ orgToHTML . T.pack $ postContent
---   case pandocReturn of
---     Left _ -> putError "BAD"
---     Right outData -> writeFile' out (T.unpack outData)
+mkGetPosts getPost =
+  newCache $ \() -> mapM getPost =<< getDirectoryFiles "" ["src/posts//*.org"]
