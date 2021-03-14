e9f98fa5dc99fefd779561d5b99c27ebeca2f612
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2021-03-14 13:42:08 +0100

    build fix

--- engine/build.sh
+++ engine/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-mkdir -p _shake
-ghc --make Shakefile.hs -rtsopts -threaded -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build "$@"
+
+# mkdir -p _shake
+# ghc --make app/Shakefile.hs -rtsopts -threaded -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build "$@"
+
+cabal v2-run -- her-esy-fun "$@"
