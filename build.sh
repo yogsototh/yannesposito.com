247c27678852f9e482a574f0f3b290c7e8c14c6f
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2021-04-18 23:21:53 +0200

    slow but work

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 # mkdir -p _shake
 # ghc --make app/Shakefile.hs -rtsopts -threaded -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build "$@"
 
-cabal v2-run -- her-esy-fun "$@"
+# cabal v2-run -- her-esy-fun "$@"
+
+runghc app/Shakefile.hs "$@"
