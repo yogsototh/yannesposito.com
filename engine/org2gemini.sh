23fc0738fc7d8e4684bfc373d820eb8d7d4a7eaa
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2021-05-02 11:26:51 +0200

    improved org2gemini

--- engine/org2gemini.sh
+++ engine/org2gemini.sh
@@ -12,6 +12,7 @@ dst="$2"
   perl -pe 's#=> file:([^ ]*)#=> $1#g;' | \
   perl -pe 's#\[\[(file:)?([^]]*)\]\]#=> $2#g;' | \
   perl -pe 's#^\* *\n\n##' | \
+  perl -pe 's#^\s*[-\*\+\.]\s*$##' | \ # remove lines with a single special char
   perl -pe 's#^\**[ ]*:.*:$##' | \
   perl -pe 's#^\s[- ]*$#\n#;' > "$dst"
 
