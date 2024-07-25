f841b92802b109ab6e61291e3c5c90fc3c912b22
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2024-07-25 23:58:42 +0200

    BRUT post + lua links fix

--- engine/links-to-html.lua
+++ engine/links-to-html.lua
@@ -1,5 +1,8 @@
 -- links-to-html.lua
 function Link(el)
-  el.target = string.gsub(string.gsub(el.target, "%.org", ".html"), "%.html::", ".html#" )
+  -- this if is necessary to not match .org domains, but only .org files.
+  if (not (string.match(el.target, "https?://"))) then
+     el.target = string.gsub(string.gsub(el.target, "%.org", ".html"), "%.html::", ".html#" )
+  end
   return el
 end
